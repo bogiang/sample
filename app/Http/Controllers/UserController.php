73c3c5757e03656931cb1e1131c4966565ca3a7d
bogiang <932921796@qq.com>
2018-04-11 19:31:31 +0800

    发送到邮件，改密码

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -104,13 +104,11 @@ class UserController extends Controller
     protected function sendEmailConfirmationTo($user){
         $view = 'emails.confirm';
         $data = compact('user');
-        $from = '932921796@qq.com';
-        $name = 'bogiang';
         $to = $user->email;
         $subject = '感谢注册 bogiang 应用！请确认你的邮箱。';
 
-        Mail::send($view, $data, function($message) use ($from, $name, $to ,$subject){
-            $message->from($from, $name)->to($to)->subject($subject);
+        Mail::send($view, $data, function($message) use ($to ,$subject){
+            $message->to($to)->subject($subject);
         });
     }
 }
